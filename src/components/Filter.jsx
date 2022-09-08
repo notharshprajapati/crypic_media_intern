3a2f8042cc239d20cc0216e5fd2cd363bfeebce8
notharshprajapati <prajapati.harsh0243@gamil.com>
2022-09-09 02:32:34 +0530

    added filter

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,17 +1,22 @@
-import React, { useEffect } from "react";
-import styled from "styled-components";
+import React, { useEffect, useState } from "react";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import ToggleButton from "react-bootstrap/ToggleButton";
 
 const Filter = ({ popular, setFiltered, activeGenre, setActiveGenre }) => {
+  const [checked, setChecked] = useState(false);
+  const [radioValue, setRadioValue] = useState("0");
+  const [curr, setCurr] = useState("0");
+
   const filters = [
-    "Anime",
-    "Children",
-    "Comedy",
-    "Drama",
-    "Horror",
-    "Legal",
-    "Medical",
-    "Music",
-    "Sports",
+    { name: "Anime", value: "1" },
+    { name: "Children", value: "2" },
+    { name: "Comedy", value: "3" },
+    { name: "Drama", value: "4" },
+    { name: "Horror", value: "5" },
+    { name: "Legal", value: "6" },
+    { name: "Medical", value: "7" },
+    { name: "Music", value: "8" },
+    { name: "Sports", value: "9" },
   ];
 
   useEffect(() => {
@@ -23,50 +28,39 @@ const Filter = ({ popular, setFiltered, activeGenre, setActiveGenre }) => {
       movie.show.genres.includes(activeGenre)
     );
     setFiltered(filtered);
+    setCurr(activeGenre.value);
   }, [activeGenre]);
 
   return (
-    <Wrapper>
-      <div className="filter-container">
-        <button
-          className={activeGenre === "All" ? "active" : ""}
+    <div>
+      <ButtonGroup className="mb-2">
+        <ToggleButton
+          type="radio"
+          variant="primary"
+          name="radio"
+          value="All"
+          checked={activeGenre === "All"}
           onClick={() => setActiveGenre("All")}
         >
           All
-        </button>
-        {filters.map((filter, i) => {
-          return (
-            <button
-              key={i}
-              className={activeGenre === filter ? "active" : ""}
-              onClick={() => setActiveGenre(filter)}
-            >
-              {filter}
-            </button>
-          );
-        })}
-      </div>
-    </Wrapper>
+        </ToggleButton>
+        {filters.map((radio, idx) => (
+          <ToggleButton
+            key={idx}
+            id={`radio-${idx}`}
+            type="radio"
+            variant="primary"
+            name="radio"
+            value={radio.value}
+            checked={activeGenre === radio.name}
+            onClick={() => setActiveGenre(radio.name)}
+          >
+            {radio.name}
+          </ToggleButton>
+        ))}
+      </ButtonGroup>
+    </div>
   );
 };
-const Wrapper = styled.div`
-  .filter-container button {
-    margin-right: 1vh;
-    margin-bottom: 1vh;
-    min-width: 5rem;
-    padding: 0.5rem 1rem;
-    border: none;
-    background: white;
-    color: rgh(65, 98, 168);
-    border-radius: 1rem;
-    border: 2px solid rgb(28, 36, 53);
-    font-weight: bold;
-    cursor: pointer;
-  }
 
-  button.active {
-    background: rgb(65, 98, 168);
-    color: white;
-  }
-`;
 export default Filter;
